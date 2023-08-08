f7b5df98bee3a5826ec78685ed8eb960354cb24c
jlbmanetti <jlbmanetti@gmail.com>
2023-08-07 21:19:09 -0300

    aa

---
+++ 3.1-fibo.py
@@ -0,0 +1,5 @@
+#import sys
+#sys.path.append("C:/Users/jlbma/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/jlbma/OneDrive/Carreira/Curso Santander Coders/2 - Programação Orientada a Objetos (PY)/Santander_Coders_PY_POO/3.1-fibo.py")
+
+import my_package.fibo as fb
+
